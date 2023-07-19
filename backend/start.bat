7eb66cf8bd8cce997fd6fdfec3d9c7ef2962b558
Miskler <lysenkomiha913@gmail.com>
2023-07-19 17:04:30 +0300

    make dir "steam_client" if not exist on start

--- backend/start.bat
+++ backend/start.bat
@@ -1,1 +1,2 @@
+if not exist "steam_client" mkdir steam_client
 uvicorn main:app --host 5.44.41.136 --port 8000
--- backend/start.sh
+++ backend/start.sh
@@ -1,1 +1,2 @@
+mkdir -p steam_client
 uvicorn main:app --host 5.44.41.136 --port 8000
