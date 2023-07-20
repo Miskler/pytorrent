a19c5b15ff4931c134407a5a05f54798f5e140f8
Miskler <lysenkomiha913@gmail.com>
2023-07-20 03:11:09 +0300

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,2 +1,2 @@
 mkdir -p steam_client
-gunicorn -b 5.44.41.136 --port 8000 -c gunicorn_config.py main:app
+gunicorn main:app -b 5.44.41.136:8000 -c gunicorn_config.py --worker-class uvicorn.workers.UvicornWorker
